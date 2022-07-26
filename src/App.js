30041fa9ea03c6f342badca3710f7fc9b26a24fa
SMABF7899 <smabanifatemi@gmail.com>
2022-07-26 10:24:15 +0430

    Login Page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Login to the Dashboard</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <input className="App-input" type="text" placeholder="Username"/><br/>
+          <input className="App-input" type="password" placeholder="Password"/><br/>
+          <input className="App-input" type="checkbox" id="checkbox" name="vehicle1" value="Bike"/>
+          <label htmlFor="checkbox"> Remember me</label><br/><br/>
+          <input className="App-button" type="button" value="  Login  "/><br/><br/>
+          <a
+              className="App-link"
+              href="http://smabf.ir"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Visit My Site
+          </a>
+        </form>
       </header>
     </div>
   );
